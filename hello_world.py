7185786991f5793ad4470d5aa46b3fdbe4ca3123
sanikamore419 <sanikaamore419@gmail.com>
2025-09-08 22:30:17 +0530

    your message

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# hello_world.py
+# First Python program for GitHub 🚀
+
+def main():
+    print("Hello, GitHub! 👋")
+    print("This is my first Python file pushed to GitHub.")
+
+if __name__ == "__main__":
+    main()
